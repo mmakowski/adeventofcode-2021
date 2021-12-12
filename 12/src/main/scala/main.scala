4d93a4734ef1b2ba9426f56790027ec7a1a944ed
Maciek Makowski <maciek@mmakowski.com>
2021-12-12 16:11:56 +0100

    solution to 12 part 2

--- 12/src/main/scala/main.scala
+++ 12/src/main/scala/main.scala
@@ -4,22 +4,40 @@ import java.io.InputStream
 
 type Node = String
 type Path = Seq[Node]
-
-def solve(input: String): Set[Path] = solve(index(parse(input)))
-
 type Edge = (Node, Node)
 
-private def solve(edges: Map[Node, Set[Node]]): Set[Path] =
-  val paths = Set(Seq("start"))
-  extend(edges, paths)
+def solve1(input: String): Set[Path] = solve1(index(parse(input)))
 
-private def extend(edges: Map[Node, Set[Node]], paths: Set[Path]): Set[Path] =
+private def solve1(edges: Map[Node, Set[Node]]): Set[Path] =
+  extend1(edges, Set(Seq("start")))
+
+private def extend1(edges: Map[Node, Set[Node]], paths: Set[Path]): Set[Path] =
   val (complete, toExtend) = paths.partition(_.endsWith(Seq("end")))
-  val extended = toExtend.flatMap(step(edges, _))
-  if (extended.isEmpty) complete else complete ++ extend(edges, extended)
+  val extended = toExtend.flatMap(step1(edges, _))
+  if (extended.isEmpty) complete else complete ++ extend1(edges, extended)
 
-private def step(edges: Map[Node, Set[Node]], path: Path): Set[Path] = edges(path.last)
+private def step1(edges: Map[Node, Set[Node]], path: Path): Set[Path] = edges(path.last)
   .filter { nextNode => isBig(nextNode) || !path.contains(nextNode) }
+  .map(path :+ _)
+
+def solve2(input: String): Set[Path] = solve2(index(parse(input)))
+
+private def solve2(edges: Map[Node, Set[Node]]): Set[Path] =
+  extend2(edges, Set(Seq("start")))
+
+private def extend2(edges: Map[Node, Set[Node]], paths: Set[Path]): Set[Path] =
+  val (complete, toExtend) = paths.partition(_.endsWith(Seq("end")))
+  val extended = toExtend.flatMap(step2(edges, _))
+  if (extended.isEmpty) complete else complete ++ extend2(edges, extended)
+
+private def step2(edges: Map[Node, Set[Node]], path: Path): Set[Path] = edges(path.last)
+  .filter { nextNode =>
+    nextNode != "start" && (
+      isBig(nextNode) ||
+        !path.contains(nextNode) ||
+        !containsMultipleSmall(path)
+      )
+  }
   .map(path :+ _)
 
 private def parse(input: String): Set[Edge] = input
@@ -37,3 +55,10 @@ private def index(edges: Set[Edge]): Map[Node, Set[Node]] = edges
   .toMap
 
 private def isBig(node: Node): Boolean = node.toUpperCase == node
+
+private def containsMultipleSmall(path: Path): Boolean = path
+  .filterNot(isBig)
+  .groupBy(identity)
+  .values
+  .map(_.size)
+  .exists(_ > 1)
--- 12/src/test/scala/Test12.scala
+++ 12/src/test/scala/Test12.scala
@@ -4,8 +4,8 @@ import org.scalatest.Assertion
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test12 extends AnyFunSuite {
-  test("basic example") {
-    testSolve(
+  test("1: basic example") {
+    testSolve1(
       """start-A
         |start-b
         |A-c
@@ -27,8 +27,8 @@ class Test12 extends AnyFunSuite {
     )
   }
 
-  test("larger example") {
-    testSolve(
+  test("1: larger example") {
+    testSolve1(
       """dc-end
         |HN-start
         |start-kj
@@ -61,8 +61,8 @@ class Test12 extends AnyFunSuite {
     )
   }
 
-  test("test example") {
-    val solution = solve(
+  test("1: test example") {
+    val solution = solve1(
       """mx-IQ
         |mx-HO
         |xq-start
@@ -88,7 +88,87 @@ class Test12 extends AnyFunSuite {
     println(solution.size)
   }
 
-  private def testSolve(input: String, expectedOutput: String): Assertion = {
+  test("2: basic example") {
+    testSolve2(
+      """start-A
+        |start-b
+        |A-c
+        |A-b
+        |b-d
+        |A-end
+        |b-end
+        |""".stripMargin,
+      """start,A,b,A,b,A,c,A,end
+        |start,A,b,A,b,A,end
+        |start,A,b,A,b,end
+        |start,A,b,A,c,A,b,A,end
+        |start,A,b,A,c,A,b,end
+        |start,A,b,A,c,A,c,A,end
+        |start,A,b,A,c,A,end
+        |start,A,b,A,end
+        |start,A,b,d,b,A,c,A,end
+        |start,A,b,d,b,A,end
+        |start,A,b,d,b,end
+        |start,A,b,end
+        |start,A,c,A,b,A,b,A,end
+        |start,A,c,A,b,A,b,end
+        |start,A,c,A,b,A,c,A,end
+        |start,A,c,A,b,A,end
+        |start,A,c,A,b,d,b,A,end
+        |start,A,c,A,b,d,b,end
+        |start,A,c,A,b,end
+        |start,A,c,A,c,A,b,A,end
+        |start,A,c,A,c,A,b,end
+        |start,A,c,A,c,A,end
+        |start,A,c,A,end
+        |start,A,end
+        |start,b,A,b,A,c,A,end
+        |start,b,A,b,A,end
+        |start,b,A,b,end
+        |start,b,A,c,A,b,A,end
+        |start,b,A,c,A,b,end
+        |start,b,A,c,A,c,A,end
+        |start,b,A,c,A,end
+        |start,b,A,end
+        |start,b,d,b,A,c,A,end
+        |start,b,d,b,A,end
+        |start,b,d,b,end
+        |start,b,end""".stripMargin
+    )
+  }
+
+  test("2: test example") {
+    val solution = solve2(
+      """mx-IQ
+        |mx-HO
+        |xq-start
+        |start-HO
+        |IE-qc
+        |HO-end
+        |oz-xq
+        |HO-ni
+        |ni-oz
+        |ni-MU
+        |sa-IE
+        |IE-ni
+        |end-sa
+        |oz-sa
+        |MU-start
+        |MU-sa
+        |oz-IE
+        |HO-xq
+        |MU-xq
+        |IE-end
+        |MU-mx""".stripMargin
+    )
+    println(solution.size)
+  }
+
+  private val testSolve1 = testSolve(solve1)
+
+  private val testSolve2 = testSolve(solve2)
+
+  private def testSolve(solve: String => Set[Path])(input: String, expectedOutput: String): Assertion = {
     val expectedPaths = expectedOutput
       .split("\n")
       .map(_.split(",").toSeq)
